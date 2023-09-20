0bcb4b9e5af380d7957152e805b2fa8fe410e1c8
AobaVN <aobachymtee@gmail.com>
2023-09-20 21:10:08 +0700

    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Có một cơn lốc đang dần <code>tới đây</code>!
         </p>
         <a
           className="App-link"
